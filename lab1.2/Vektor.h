202ac21b19f667b0c88fd85d092ee88905e47d0b
NotOriginal333 <m.naumovych2@gmail.com>
2024-02-17 10:57:58 +0200

    no_set

--- lab1.2/Vektor.h
+++ lab1.2/Vektor.h
@@ -5,10 +5,21 @@ private:
 	float* flo;
 	int i;
 	int state;
+
+	static int objectCount;
 public:
 	void init();
 	void init(int n);
 	void init(int n, float value);
 
+	void Display();
+
+	float Max();
+	float Min();
+
+	void SortAscending();
+	void SortDescending();
+
+	static int ObjectCount();
 };
 
